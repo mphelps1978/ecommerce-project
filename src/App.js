c76ec7cbaa65f999de5f44d9941c62e726962097
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-04-30 00:10:04 -0500

    added header, styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/HomePage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import './App.css'
 
 
@@ -9,6 +10,7 @@ import './App.css'
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path = '/' component = {HomePage} />
         <Route path = '/shop' component = {ShopPage} />
