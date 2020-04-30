e1c1e70f694129c97c8600e46c11a197e7388bd9
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-04-29 22:23:28 -0500

    added styling to collection preview and individual items in the preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss'
 
 
@@ -11,13 +13,14 @@ const CollectionPreview = ({title, items}) => {
     <div className = 'collection-preview'>
       <h1>{title.toUpperCase()}</h1>
       <div className = 'preview'>
-        {
-          items
+        {items
           .filter((item, idx) => idx < 4 )
-          .map(item => (
-            <div key = {item.id}>{item.name}</div>
+          .map(({id, ...otherItemProps}) => (
+            <CollectionItem
+              key = {id}
+              {...otherItemProps}/>
           ))
-        }
+          }
       </div>
     </div>
   )
