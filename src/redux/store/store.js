81aee27078e4907b5d6760f671a1b93a441ee8cc
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-05-02 16:41:50 -0500

    connect userReducer and Actions to header and app components, connet to store for signin

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
 
 import rootReducer from './../reducers/root-reducer'
 
 const middlewares = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
-
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(...middlewares)
+))
 
 export default store
