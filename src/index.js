2532dfb51a793f1b89fe004514823bede7560d5a
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-05-02 16:02:33 -0500

    set up redux, react-redux and redux-logger. Set up initial store, and users reducer/actions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store/store'
 import App from './App';
 
+
+
+
+
 ReactDOM.render(
+  <Provider store = {store}>
   <Router>
     <App />
-  </Router>,
+  </Router>
+  </Provider>,
   document.getElementById('root')
 );
