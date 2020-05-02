50701d5ea40e6c8c9be35ba29a952c4dabcdb6c4
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-05-02 01:06:33 -0500

    working on signup form currently - going to bed

--- src/App.js
+++ src/App.js
@@ -32,12 +32,7 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          },
-          () => {
-            console.log(this.state);
-
-          }
-          )
+          })
         })
 
 
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
+
+import './sign-up.styles.scss'
+
+
+class SignUp extends React.Component {
+
+ constructor(){
+   super()
+
+   this.state = {
+     displayName: '',
+     email: '',
+     password: '',
+     confirmPassword: ''
+   }
+ }
+
+ render() {
+  const { displayName, email, password, confirmPassword } = this.this.state
+
+  return(
+    <div classname = 'sign-up'>
+      <h2 className = 'title'>I do not have an account</h2>
+      <span>Sign up with your email and password</span>
+
+      <form className = 'sign-up-form' onSubmit = {this.handleSubmit}>
+        <FormInput
+          type = 'text'
+          name = 'displayName'
+          value = { displayName }
+          onChange = {this.handleChange}
+          label = 'Display Name'
+          required
+        >
+        </FormInput>
+
+      </form>
+    
+    </div>
+  )
+
+ }
+
+}
