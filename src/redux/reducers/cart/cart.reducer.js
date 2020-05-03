0213adf5dd2b796446b3a4c0e80796e775b5d3ee
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-05-03 01:02:52 -0500

    added functionality to combine multiple items into one quantity property

--- src/redux/reducers/cart/cart.reducer.js
+++ src/redux/reducers/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import  CartActionTypes from '../../types/cart/cart.types'
+import { addItemToCart } from '../../utils/cart.utils'
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden
+      }
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
       }
     default:
       return state
